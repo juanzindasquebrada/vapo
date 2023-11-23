205face727991897889f8e90d7fe61e64cfdfa22
juanzindasquebrada <143668972+juanzindasquebrada@users.noreply.github.com>
2023-11-22 21:41:53 -0300

    Add files via upload

---
+++ Jogo/src/Jogo.java
@@ -0,0 +1,24 @@
+public class Jogo {
+    public class SuperPoder{
+        private String nome;
+        private int categoria;
+
+        public String getNome() {
+            return nome;
+        }
+
+        public int getCategoria() {
+            return categoria;
+        }
+
+        public class Personagem{
+            private String nome;
+            private String NomeVidaReal;
+            private String poder[] = new String[4];
+
+
+        }
+
+    }
+
+}
